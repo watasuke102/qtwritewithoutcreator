1a119747677d03e4f5e51acbae89c40e096e9189
watasuke102 <watasuke102@gmail.com>
2020-09-25 22:54:47 +0900

    QtDockWidget

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
-#include <QApplication>
-#include <QMainWindow>
 #include <QtWidgets>
 
-class MainWindow : public QWidget//QMainWindow
+class widget_main : public QWidget
+{
+    public:
+        widget_main(QWidget *parent = nullptr) : QWidget(parent)
+        {
+            QHBoxLayout *layout = new QHBoxLayout;
+            layout->addWidget(new QLabel(tr("THIS IS CENTRAL WIDGET\nHELLO!!")));
+            setLayout(layout);
+        }
+};
+
+class widget_A : public QWidget
 {
     private:
         int push_count = 0;
@@ -12,22 +21,77 @@ class MainWindow : public QWidget//QMainWindow
         void onButtonClicked()
         {
             push_count++;
-            label1->setText(tr("this is text (button has pushed!)\ncount: ") + QString::number(push_count) );
+            label1->setText(tr("Widget A (button has pushed!)\ncount: ") + QString::number(push_count) );
         }
 
     public:
-        MainWindow(QWidget *parent = nullptr) : QWidget(parent)
-        {
-            label1 = new QLabel(tr("this is text (button has not pushed)"));
+        widget_A(QWidget *parent = nullptr) : QWidget(parent)
+{
+            label1 = new QLabel(tr("Widget A (button has not pushed)"));
             button = new QPushButton(tr("Let's push"));
-            QObject::connect(button, &QPushButton::clicked, this, &MainWindow::onButtonClicked);
+            QObject::connect(button, &QPushButton::clicked, this, &widget_A::onButtonClicked);
 
             QGridLayout *grid = new QGridLayout;
             grid->addWidget(label1, 0, 0);
-            grid->addWidget(button, 0, 1);
+            grid->addWidget(button, 1, 0);
             setLayout(grid);
 
-            resize(400, 300);
+            resize(300, 200);
+            setWindowTitle(tr("Qt widget A"));
+        }
+};
+
+class widget_B : public QWidget
+{
+    private:
+        int push_count = 0;
+        QLabel      *label1;
+        QPushButton *button;
+    public slots:
+        void onButtonClicked()
+        {
+            push_count++;
+            label1->setText(tr("Widget B (button has pushed!)\ncount: ") + QString::number(push_count) );
+        }
+
+    public:
+        widget_B(QWidget *parent = nullptr) : QWidget(parent)
+        {
+            label1 = new QLabel(tr("Widget B (button has not pushed)"));
+            button = new QPushButton(tr("Let's push"));
+            QObject::connect(button, &QPushButton::clicked, this, &widget_B::onButtonClicked);
+
+            QGridLayout *grid = new QGridLayout;
+            grid->addWidget(label1, 0, 0);
+            grid->addWidget(button, 1, 0);
+            setLayout(grid);
+
+            resize(300, 200);
+            setWindowTitle(tr("Qt widget A"));
+        }
+};
+
+class MainWindow : public QMainWindow
+{
+
+    public:
+        MainWindow(QWidget *parent = nullptr)
+        {
+            QDockWidget *widgetA = new QDockWidget(tr("dock"));
+            widgetA->setWidget(new widget_A);
+            widgetA->resize(500, 200);
+            widgetA->setWindowTitle(tr("Widget A"));
+            addDockWidget(Qt::LeftDockWidgetArea,  widgetA);
+
+            QDockWidget *widgetB = new QDockWidget(tr("dock"));
+            widgetB->setWidget(new widget_B);
+            widgetB->resize(300, 200);
+            widgetB->setWindowTitle(tr("Widget B"));
+            addDockWidget(Qt::RightDockWidgetArea, widgetB);
+
+            setCentralWidget(new widget_main);
+
+            resize(800, 200);
             setWindowTitle(tr("Qt system window"));
         }
 };
