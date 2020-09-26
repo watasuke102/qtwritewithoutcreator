fbe5c0e35fc6fc5d3850beb0c2f1bf782bff8d64
watasuke102 <watasuke102@gmail.com>
2020-09-26 21:24:23 +0900

    OGLWidgetで図形描画出来るように

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,58 @@
 #include <QtWidgets>
+#include <GL/glu.h>
+#include <GL/gl.h>
+
+class OGLWidget : public QOpenGLWidget
+{
+public:
+    OGLWidget(QWidget *parent = nullptr) : QOpenGLWidget(parent)
+    {
+    }
+
+protected:
+    void initializeGL()
+    {
+        glClearColor(0,0,0,1);
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_LIGHT0);
+        glEnable(GL_LIGHTING);
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+        glEnable(GL_COLOR_MATERIAL);
+    }
+
+    void paintGL()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glColor4f(0, 128, 0, 0);
+        glBegin(GL_POLYGON);
+            glVertex3d(-2.0, -2.0, 0.0);
+            glVertex3d( 2.0, -2.0, 0.0);
+            glVertex3d( 2.0,  2.0, 0.0);
+        glEnd();
+    }
+
+    void resizeGL(int w, int h)
+    {
+        glViewport(0,0,w,h);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45, (float)w/h, 0.01, 100.0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(0,0,5,0,0,0,0,1,0);
+    }
+};
+
 
 class widget_main : public QWidget
 {
     public:
         widget_main(QWidget *parent = nullptr) : QWidget(parent)
         {
-            QHBoxLayout *layout = new QHBoxLayout;
-            layout->addWidget(new QLabel(tr("THIS IS CENTRAL WIDGET\nHELLO!!")));
+            QVBoxLayout *layout = new QVBoxLayout;
+            layout->addWidget(new OGLWidget);
+            layout->addWidget(new QScrollBar(Qt::Horizontal));
             setLayout(layout);
         }
 };
@@ -26,7 +72,7 @@ class widget_A : public QWidget
 
     public:
         widget_A(QWidget *parent = nullptr) : QWidget(parent)
-{
+        {
             label1 = new QLabel(tr("Widget A (button has not pushed)"));
             button = new QPushButton(tr("Let's push"));
             QObject::connect(button, &QPushButton::clicked, this, &widget_A::onButtonClicked);
@@ -36,7 +82,6 @@ class widget_A : public QWidget
             grid->addWidget(button, 1, 0);
             setLayout(grid);
 
-            resize(300, 200);
             setWindowTitle(tr("Qt widget A"));
         }
 };
@@ -66,7 +111,6 @@ class widget_B : public QWidget
             grid->addWidget(button, 1, 0);
             setLayout(grid);
 
-            resize(300, 200);
             setWindowTitle(tr("Qt widget A"));
         }
 };
@@ -79,19 +123,17 @@ class MainWindow : public QMainWindow
         {
             QDockWidget *widgetA = new QDockWidget(tr("dock"));
             widgetA->setWidget(new widget_A);
-            widgetA->resize(500, 200);
             widgetA->setWindowTitle(tr("Widget A"));
-            addDockWidget(Qt::LeftDockWidgetArea,  widgetA);
+            addDockWidget(Qt::LeftDockWidgetArea, widgetA);
 
             QDockWidget *widgetB = new QDockWidget(tr("dock"));
             widgetB->setWidget(new widget_B);
-            widgetB->resize(300, 200);
             widgetB->setWindowTitle(tr("Widget B"));
-            addDockWidget(Qt::RightDockWidgetArea, widgetB);
+            addDockWidget(Qt::BottomDockWidgetArea, widgetB);
+            resizeDocks({widgetB}, {250}, Qt::Vertical);
 
             setCentralWidget(new widget_main);
-
-            resize(800, 200);
+            resize(1200, 675);
             setWindowTitle(tr("Qt system window"));
         }
 };
